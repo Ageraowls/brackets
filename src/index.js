3076c538fb5a3231bfaf872d18289bf2788eeaf0
Andrew Shumeiko <shumeiko.drew@gmail.com>
2022-02-21 23:59:51 +0500

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  const BRACKETS_PAIR = Object.fromEntries(bracketsConfig);
+
+  const strToArr = str.split('');
+  let stack = [];
+
+  strToArr.map((item) => {
+    if (BRACKETS_PAIR[stack[stack.length - 1]] === item) {
+      stack.pop(item);
+    } else {
+      stack.push(item);
+    }
+  });
+
+  return stack.length === 0;
+};
